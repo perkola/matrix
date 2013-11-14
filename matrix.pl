197aa9b18912df08a398b43a3804ba07159018f4
William Perkola <perkola.william@gmail.com>
2013-11-14 15:05:21 +0100

    Add transpose predicate to matrix.pl

--- matrix.pl
+++ matrix.pl
@@ -9,19 +9,40 @@ zero_matrix(N, K) :-
 %%  zero_matrix(+Rows, +Columns, ?Matrix) is det
 %
 %   Generates a zero matrix K with n rows and m zeros in each row.
-%   Uses zero_list/2 to generate each row of zero vectors.
+%   Uses zero_vector/2 to generate each row of zero vectors.
 
 zero_matrix(0, _, []) :- !.
 zero_matrix(N, M, [L|K]) :-
     N1 is N - 1,
-    zero_list(M, L),
+    zero_vector(M, L),
     zero_matrix(N1, M, K).
 
-%%  zero_list(+Size, ?List) is det
+%%  zero_vector(+Size, ?List) is det
 %
-%   Generates a m long list containing only zeros.
+%   Generates a m long vector containing only zeros.
 
-zero_list(0, []) :- !.
-zero_list(M, [0|L]) :-
+zero_vector(0, []) :- !.
+zero_vector(M, [0|L]) :-
     M1 is M - 1,
-    zero_list(M1, L).
+    zero_vector(M1, L).
+
+%%  transpose(+Matrix, ?Transpose) is det
+%
+%   Transposes a matrix (squared or rectangular).
+%   Uses transpose/3 and first_column/3 with an accumulator.
+
+transpose([R|A], At) :-
+    transpose(R, [R|A], At).
+
+%   transpose(+Matrix, +Accumulator, ?Transpose)
+
+transpose([], _, []).
+transpose([_|Rs], Rest, [Rt|At]) :-
+    first_column(Rest, Rt, NewRest),
+    transpose(Rs, NewRest, At).
+
+%   first_column(+Matrix, ?List, ?Rest)
+
+first_column([], [], []).
+first_column([[E|R]|Rs], [E|Acc], [R|Rest]) :-
+    first_column(Rs, Acc, Rest).
