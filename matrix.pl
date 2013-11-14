6d144915da51c115d100a92990469b094d8711a5
William Perkola <perkola.william@gmail.com>
2013-11-14 13:13:01 +0100

    Update documentation

--- matrix.pl
+++ matrix.pl
@@ -1,6 +1,7 @@
 %%  zero_matrix(+Dimension, ?Matrix) is det
 %
 %   Generates a square zero matrix K with dimension n*n.
+%   Uses zero_matrix/3 with both n and m equal.
 
 zero_matrix(N, K) :-
     zero_matrix(N, N, K).
@@ -8,6 +9,7 @@ zero_matrix(N, K) :-
 %%  zero_matrix(+Rows, +Columns, ?Matrix) is det
 %
 %   Generates a zero matrix K with n rows and m zeros in each row.
+%   Uses zero_list/2 to generate each row of zero vectors.
 
 zero_matrix(0, _, []) :- !.
 zero_matrix(N, M, [L|K]) :-
@@ -15,7 +17,9 @@ zero_matrix(N, M, [L|K]) :-
     zero_list(M, L),
     zero_matrix(N1, M, K).
 
-%   zero_list(+Size, ?List)
+%%  zero_list(+Size, ?List) is det
+%
+%   Generates a m long ling containing only zeros.
 
 zero_list(0, []) :- !.
 zero_list(M, [0|L]) :-
